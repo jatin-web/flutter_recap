872233281c3ac1c3be704c2e3e27be19086b3803
Jatin Saini <sainijatin5940@gmail.com>
2024-05-12 01:29:22 +0530

    add description in get it tutorial

--- lib/presentation/screens/learn_get_it_service_locator.dart
+++ lib/presentation/screens/learn_get_it_service_locator.dart
@@ -2,9 +2,10 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_recap/data/repo/test_repo.dart';
+import 'package:flutter_recap/presentation/widgets/containers/note_container.dart';
 import 'package:get_it/get_it.dart';
 
-/// get_it helps us to use a single instance of services/objects. 
+/// get_it helps us to use a single instance of services/objects.
 /// It helps in improving the performance of the app
 class LearnGetItServiceLocator extends StatelessWidget {
   static const String routeName = 'get_it_locator';
@@ -14,13 +15,18 @@ class LearnGetItServiceLocator extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('GetIt Service Locator'),
+          title: const Text('Get It Service Locator'),
         ),
-        body: SizedBox(
+        body: Container(
           width: double.infinity,
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              const NoteContainer(
+                  body:
+                      "This teaches us how get_it helps us in using a single instance of a service all across the app.\n\nThis is helpful in improving the performance of the app"),
+              const SizedBox(height: 50),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
